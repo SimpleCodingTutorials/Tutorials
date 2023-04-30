0f1eafd887bbfbd501c48929511478393969b3f7
SimpleCodingTutorials <128899490+SimpleCodingTutorials@users.noreply.github.com>
2023-04-30 23:34:59 +0430

    delete

--- Hello World !/main.js
+++
@@ -1,7 +0,0 @@
-
-const message = document.getElementById("message");
-message.addEventListener("click",onClick);
-message.innerHTML="Hello World!"
-function onClick() {
-    message.innerHTML="You clicked on me!"
-}
