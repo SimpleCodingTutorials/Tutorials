7ff7d5b971ae28b68444b6b305d1f92e0068e8eb
SimpleCodingTutorials <128899490+SimpleCodingTutorials@users.noreply.github.com>
2024-04-05 15:15:26 +0330

    upload

---
+++ compress files/main.js
@@ -0,0 +1,45 @@
+const fs = require("fs-extra");
+const Minizip = require("minizip-asm.js");
+
+async function zipDirectory(source,output,password) {
+    const mz = new Minizip();
+    const files = await fs.readdir(source);
+
+    for(let file of files) {
+        const filePath = `${source}/${file}`;
+        const fileData = await fs.readFile(filePath);
+        mz.append(file,fileData,{password: password});
+    }
+
+    const data = new Uint8Array(mz.zip());
+    await fs.writeFile(output,data);
+}
+
+zipDirectory("./zipped","./output.zip","123456");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
