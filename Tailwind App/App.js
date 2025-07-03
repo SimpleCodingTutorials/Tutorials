fea32da1965b53180d4d8477093bdde2ecadfd02
SimpleCodingTutorials <128899490+SimpleCodingTutorials@users.noreply.github.com>
2025-07-03 13:50:31 +0330

    upload

---
+++ Tailwind App/App.js
@@ -0,0 +1,30 @@
+import {View,Text} from "react-native";
+import "./global.css";
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white" >
+      <Text className="text-3xl font-bold text-green-600" >Welcome to the app!</Text>
+    </View>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
