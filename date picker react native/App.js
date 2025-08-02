26d84ce86cf8fcf6e016f2f861559c004ec622cf
SimpleCodingTutorials <128899490+SimpleCodingTutorials@users.noreply.github.com>
2025-08-02 14:22:59 +0330

    upload

--- date picker react native/App.js
+++ date picker react native/App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Button, Text, Platform } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default function App() {
+  const [date, setDate] = useState(new Date());
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    setShow(Platform.OS === 'ios');
+    if (selectedDate) setDate(selectedDate);
+  };
+
+  const showMode = (currentMode) => {
+    setMode(currentMode);
+    setShow(true);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+  <View style={{ flexDirection: 'row', gap: 10 }} >
+    <Button title="Pick Date" onPress={() => showMode('date')} />
+    <Button title="Pick Time" onPress={() => showMode('time')} />
+  </View>
+
+  <Text style={{ marginTop: 20 }}>{date.toLocaleString()}</Text>
+      {show && (
+        <DateTimePicker
+          value={date}
+          mode={mode}
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+        />
+      )}
+</View>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
